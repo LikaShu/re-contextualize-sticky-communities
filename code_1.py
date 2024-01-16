aac23dafc337936f2d467129179cf9e4509c82de
LikaShu <142459648+LikaShu@users.noreply.github.com>
2024-01-16 17:04:02 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,29 @@
+    }
+
+            System.out.println("Random Number: " + item);
+        }
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+
+}
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+        int[] data = new int[10];
+        }    const data = [];
+
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+
+}
+main();
+function main() {
+    }
+}
+        console.log("Random Number: " + item);
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+    return data;
+    });
+    data.forEach(item => {
